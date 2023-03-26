d40b107085c6545a8dc2680df2311ba5d964e081
art3mes <93867313+art3mes@users.noreply.github.com>
2023-03-26 15:12:45 +0530

    add behavior next

--- assets/CSS-JS/main.js
+++ assets/CSS-JS/main.js
@@ -1,9 +1,2 @@
 console.log("l;,mao");
-const phoneNumberInput = document.querySelector('input[type="number"]');
 
-phoneNumberInput.addEventListener('invalid', function() {
-  this.setCustomValidity(''); // remove default validation message
-  if (!this.validity.valid) {
-    this.setCustomValidity('This field is required.');
-  }
-});
