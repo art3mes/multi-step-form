ebac818418d5b87c9d169fc7d0cf6ad0eb498f95
art3mes <93867313+art3mes@users.noreply.github.com>
2023-03-27 15:57:19 +0530

    step 2

--- assets/CSS-JS/main.js
+++ assets/CSS-JS/main.js
@@ -1,2 +1,16 @@
 console.log("l;,mao");
+const heading="Select your plan";
+const subHeading="You have the option of monthly or yearly billing.";
 
+function handleSubmit(event){
+    event.preventDefault();
+    console.log("ok");
+    // $(".button-left").css('display','flex');     //go back button reappear
+
+    $(".header-top").text(heading);
+    $(".header-bottom").text(subHeading);
+
+    $(".step1-body").css('display','none');
+    $(".global").css('display','flex');
+    
+}
