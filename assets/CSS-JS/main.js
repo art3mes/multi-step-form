62610690a20a3ceffd76e1bfb276cb4f7b108763
art3mes <93867313+art3mes@users.noreply.github.com>
2023-03-29 16:09:43 +0530

    step 3 added. no js

--- assets/CSS-JS/main.js
+++ assets/CSS-JS/main.js
@@ -1,14 +1,17 @@
 console.log("l;,mao");
-const heading="Select your plan";
-const subHeading="You have the option of monthly or yearly billing.";
+const heading_step2="Select your plan";
+const subHeading_step2="You have the option of monthly or yearly billing.";
+
+const heading_step3="Pick add-ons";
+const subHeading_step3="Add-ons help enhance your gaming experience";
 
 function handleSubmit(event){
     event.preventDefault();
     console.log("ok");
     // $(".button-left").css('display','flex');     //go back button reappear
 
-    $(".header-top").text(heading);
-    $(".header-bottom").text(subHeading);
+    $(".header-top").text(heading_step2);
+    $(".header-bottom").text(subHeading_step2);
 
     $(".step1-body").css('display','none');
     $(".step2-body").css('display','flex');
